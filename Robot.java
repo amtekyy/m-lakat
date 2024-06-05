3711e9b97b9796ac13255d703fa76b88ea7fc6b0
amtekyy <ahmetefe2536@gmail.com>
2024-06-05 19:46:14 +0300

    mulakat

---
+++ Robot.java
@@ -0,0 +1,106 @@
+package frc.robot;
+
+//FRC
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Timer;
+
+//REV
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class Robot extends TimedRobot {
+
+  double maxSpeed = 0.5;
+
+  private final XboxController controller = new XboxController(0);
+
+  private final CANSparkMax leftMotor = new CANSparkMax(9, MotorType.kBrushed);
+  private final CANSparkMax leftFollowing = new CANSparkMax(8, MotorType.kBrushed);
+  private final CANSparkMax rightMotor = new CANSparkMax(7, MotorType.kBrushed);
+  private final CANSparkMax rightFollowing = new CANSparkMax(6, MotorType.kBrushed);
+  
+  private final DifferentialDrive robot = new DifferentialDrive(leftMotor, rightMotor);
+
+  private final CANSparkMax shooter = new CANSparkMax(5, MotorType.kBrushed);
+  private final CANSparkMax shooterFollowing = new CANSparkMax(4, MotorType.kBrushed);
+
+  private final Timer timer = new Timer();
+
+  @Override
+  public void robotInit() {
+    leftFollowing.follow(leftMotor);
+    rightFollowing.follow(rightMotor);
+    
+    rightMotor.setInverted(true);
+
+    shooterFollowing.follow(shooter);
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {
+    timer.reset();
+    timer.start();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    if (timet.get()<=3) {
+      robot.arcadeDrive(0.5, 0)
+      shooter.set(-0.5)
+     } if (timer.get()>3 && timer.get()<=5) {
+            robot.arcadeDrive(-0.5, 0)
+            shooter.set(0)
+      } if (timet.get()>5 && timer.get()<=1) {
+            robot.arcadeDrive(0, 0.5)
+            shooter.set(1)
+          }
+    //timer.get() ile o anki süreyi alırsınız.
+    //shooter.set() ile shooter motorunun hızını ayarlarsınız (-1 ile 1 arasında değer alır)
+    //robot.arcadeDrive() 1. Değer: x Yönündeki Hızı (ileri geri 1/-1), 2. Değer: y Dönüş Hızı(sağa sola dönüş 1/-1))
+  }
+
+  @Override
+  public void teleopInit() {}                                                   
+
+  @Override
+  public void teleopPeriodic() {
+    if (controller.getLeftBumper()) {
+      maxSpeed=0.6;
+    } if (controller.getRightBumper()) {
+      maxSpeed=0.8;
+    } if (controller.getRawButtonPressed(1)) {
+      shooter.set(-1);
+    } if (controller.getRawButtonReleased(1)) {
+      shooter.set(0);
+    } if (controller.getRawButtonPressed(4)) {
+      shooter.set(0.5);
+    } if (controller.getRawButtonReleased(4)) {
+      shooter.set(0);
+    }
+
+    robot.arcadeDrive(-controller.getLeftY()*maxSpeed, controller.getRightX()*maxSpeed);
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
